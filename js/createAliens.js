4c6df9ceee223fe65f861297ff6e747da7991a8b
Ayoub Outrgua <74433630+Ayoub-Outrgua@users.noreply.github.com>
2026-01-06 20:12:53 +0000

    update aliens and player content

--- js/createAliens.js
+++ js/createAliens.js
@@ -1,7 +1,14 @@
 export const createAliens = () => {
     const aliensGrid = document.getElementById('aliens-grid');
-    for (let i = 0; i < 28; i++) {
+    for (let i = 1; i <= 60; i++) {
         const alien = document.createElement('div')
+        if (i > 0 && i <= 12) {
+            alien.style.backgroundImage = "url('./assets/green.png')"
+        } else if (i > 12 && i <= 36) {
+            alien.style.backgroundImage = "url('./assets/yellow.png')"
+        } else if (i > 36) {
+            alien.style.backgroundImage = "url('./assets/red.png')"
+        }
         alien.classList.add('alien')
         aliensGrid.appendChild(alien)
     }
