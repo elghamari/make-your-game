e8e0593b0168bc452f238b15b31448c2648d59b1
elghamari <mohammed.elghamari@gmail.com>
2026-01-08 17:31:54 +0100

    update main

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 import { createAliens } from "./createAliens.js";
 import { moveAliens } from "./moveAliens.js";
 import { movePlayer } from "./movePlayer.js";
+import { toggleView } from "./toggleView.js"; 
 
-createAliens()
+
 export const gameLoop = () => {
-    moveAliens()
-    movePlayer()
-    requestAnimationFrame(gameLoop)
-}
-requestAnimationFrame(gameLoop)
+    moveAliens();
+    movePlayer();
+    requestAnimationFrame(gameLoop);
+};
+
+
+const startGame = () => {
+    console.log("Game Started!"); 
+    createAliens(); 
+    requestAnimationFrame(gameLoop); 
+};
+
+toggleView(startGame);
