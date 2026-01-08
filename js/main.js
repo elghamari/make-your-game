5a97292b3f7ab3c985be95622b8a0d7c58cf5711
aoutrgua <outrgua1949@gmail.com>
2026-01-08 17:44:04 +0100

    update moveAliens

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
+console.log("befor ----------------");
+
 import { createAliens } from "./createAliens.js";
-import { moveAliens } from "./moveAliens.js";
+import { moveAliens, moveAlienExtra } from "./moveAliens.js";
 import { movePlayer } from "./movePlayer.js";
+console.log("after +++++++++++++++++");
+
 
 createAliens()
-export const gameLoop = () => {
+export const gameLoop = (time) => {
     moveAliens()
     movePlayer()
+    moveAlienExtra(time)
     requestAnimationFrame(gameLoop)
 }
 requestAnimationFrame(gameLoop)
--- js/moveAliens.js
+++ js/moveAliens.js
@@ -1,3 +1,5 @@
+import { getAlienExtra } from "./createAliens.js";
+
 const board = document.getElementById('game-board')
 const aliensGrid = document.getElementById('aliens-grid')
 let x = 0, y = 0, direction = 1, speed = 2, dropStep = 20
@@ -24,4 +26,34 @@ export const moveAliens = () => {
     }
     aliensGrid.style.left = x + "px"
     aliensGrid.style.top = y + "px"
+}
+
+let extraX = 0, extraDirection = 1, extraActive = false, extraSpeed = 0, lastExtraTime = 0
+
+export const moveAlienExtra = (time) => {
+    const alienExtra = getAlienExtra()
+    const boardWidth = board.offsetWidth
+    const alienExtraWidth = alienExtra.offsetWidth
+    if (!extraActive && time - lastExtraTime >= 6000) {
+        lastExtraTime = time
+        extraActive = true
+
+        const fromLeft = Math.random() < 0.5
+        extraDirection = fromLeft ? 1 : -1
+
+        extraX = fromLeft ? -alienExtraWidth : boardWidth
+        alienExtra.style.display = "block"
+
+        extraSpeed = (boardWidth + alienExtraWidth) / (5 * 60)
+    }
+
+    if (!extraActive) return
+
+    extraX += extraDirection * extraSpeed
+    alienExtra.style.left = extraX + "px"
+
+    if (extraX > boardWidth + alienExtraWidth || extraX < -alienExtraWidth * 2) {
+        extraActive = false
+        alienExtra.style.display = "none"
+    }
 }
