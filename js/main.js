0f408d2809c76fabef706a9b3e2b27668ba8d624
elghamari <mohammed.elghamari@gmail.com>
2026-01-11 18:58:38 +0100

    update move player

--- js/main.js
+++ js/main.js
@@ -3,19 +3,24 @@ import { moveAliens, moveAlienExtra } from "./moveAliens.js";
 import { movePlayer, initPlayerPosition } from "./movePlayer.js";
 import { toggleView } from "./toggleView.js"; 
 import { createBunker } from "./createBunker.js" 
+import { moveLasers, shoot } from "./laser.js";
+import { keys } from "./movePlayer.js";
 
 
 export const gameLoop = (time) => {
     moveAliens();
     movePlayer();
     moveAlienExtra(time)
+    if (keys[" "]) {
+        shoot(time);
+    }
+    moveLasers();
     requestAnimationFrame(gameLoop);
 };
 
 
 const startGame = () => {
     console.log("Game Started!"); 
-    createAliens(); 
     initPlayerPosition();
     createAliens();
     createBunker()
