b3ff7ac9bb626fbb8379c5125c92f3b83076e01b
aoutrgua <outrgua1949@gmail.com>
2026-01-11 16:30:34 +0100

    update game ui

--- js/toggleView.js
+++ js/toggleView.js
@@ -1,11 +1,11 @@
 export function toggleView(onStart) {
     const startScreen = document.getElementById('start-screen');
-    const gameBoard = document.getElementById('game-board');
+    const gameBoard = document.getElementById('screen');
     const startButton = document.querySelector('.play-action p');
 
     startButton.addEventListener('click', () => {
         startScreen.style.display = 'none';
-        gameBoard.style.display = 'flex';
+        gameBoard.style.display = 'block';
         onStart(); 
     });
 }
