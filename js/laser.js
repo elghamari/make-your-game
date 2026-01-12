2daf1cc7c8059a16b279562e389ee4cb97aa2937
elghamari <mohammed.elghamari@gmail.com>
2026-01-12 17:42:22 +0100

    add feature to know points of which aliens is dead

--- js/laser.js
+++ js/laser.js
@@ -1,6 +1,6 @@
 import { player, board } from "./movePlayer.js";
 import { aliens, getAlienExtra } from "./createAliens.js";
-
+import { addScore } from "./gameState.js";
 
 const laserSpeed = 10;
 const cooldownTime = 1000;
@@ -8,8 +8,8 @@ let lastShootTime = 0;
 const lasers = []
 
 
-const checkCollision = (rect1,rect2) => {
-    return (rect1.left < rect2.right 
+const checkCollision = (rect1, rect2) => {
+    return (rect1.left < rect2.right
         && rect1.right > rect2.left
         && rect1.top < rect2.bottom
         && rect1.bottom > rect2.top
@@ -20,7 +20,7 @@ export const shoot = (time) => {
         const laser = document.createElement("div");
         laser.classList.add("laser");
         board.append(laser)
-        let coordX = player.offsetLeft + (player.offsetWidth / 2 ) - (laser.offsetWidth / 2 )
+        let coordX = player.offsetLeft + (player.offsetWidth / 2) - (laser.offsetWidth / 2)
         let coordY = player.offsetTop;
         laser.style.left = coordX + "px";
         laser.style.top = coordY + "px"
@@ -30,41 +30,45 @@ export const shoot = (time) => {
     }
 }
 
-export const moveLasers = ()=> {
-    for (let i = lasers.length-1; i >=0 ; i--) {
+export const moveLasers = () => {
+    for (let i = lasers.length - 1; i >= 0; i--) {
         let laser = lasers[i];
         let newTop = laser.offsetTop - laserSpeed;
         laser.style.top = newTop + "px";
-        if(newTop < 0) {
+        if (newTop < 0) {
             laser.remove();
-            lasers.splice(i,1);
+            lasers.splice(i, 1);
             continue;
         }
         let rectLaser = laser.getBoundingClientRect();
         for (let j = 0; j < aliens.length; j++) {
-            let alien = aliens[j];
-            let rectAlien = alien.getBoundingClientRect();
-            if(checkCollision(rectLaser,rectAlien)) {
+            let alienData = aliens[j];
+            let alienDiv = alienData.element;
+            let rectAlien = alienDiv.getBoundingClientRect();
+
+            if (checkCollision(rectLaser, rectAlien)) {
                 console.log("BOM");
                 // alien.remove();
-                alien.style.visibility = "hidden";
-                aliens.splice(j,1);
+                alienDiv.style.visibility = "hidden";
+                aliens.splice(j, 1);
                 laser.remove();
-                lasers.splice(i,1);
-                break
+                lasers.splice(i, 1);
+                addScore(alienData.points);
+                break;
             }
         }
         let alienExtra = getAlienExtra();
-        if(lasers.includes(laser) && alienExtra  && alienExtra.style.display === "block") {
+        if (lasers.includes(laser) && alienExtra && alienExtra.style.display === "block") {
             let rectAlienExtra = alienExtra.getBoundingClientRect();
-            if(checkCollision(rectLaser,rectAlienExtra)) {
+            if (checkCollision(rectLaser, rectAlienExtra)) {
                 alienExtra.style.display = "none";
                 laser.remove();
-                lasers.splice(i,1);
+                lasers.splice(i, 1);
                 console.log("extra hit");
-                
+                let point = Math.floor(Math.random() * 200) + 100;
+                addScore(point);
+                console.log(point);
             }
         }
     }
 }
-
