814e751c1d17e4440770bd6cf2af9ca15c976b88
elghamari <mohammed.elghamari@gmail.com>
2026-01-13 18:06:50 +0100

    update some issues

--- js/updateLives.js
+++ js/updateLives.js
@@ -1,5 +1,6 @@
 const lives = document.getElementById('lives')
 export const createLives = () => {
+    lives.innerHTML = "";
     for (let i = 0; i < 3; i++) {
         const live = document.createElement('div')
         live.id = 'live'
