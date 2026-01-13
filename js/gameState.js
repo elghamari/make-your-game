eb489d9bc3b5c4af5518d6491f7f60984a86d553
elghamari <mohammed.elghamari@gmail.com>
2026-01-13 18:04:44 +0100

    create function to rest score and mange state to know ispaused or not

--- js/gameState.js
+++ js/gameState.js
@@ -1,8 +1,26 @@
 let score = 0;
-
+let isPaused = false;
 const scoreElement = document.querySelector(".score");
-
+const pauseScreen = document.querySelector("#pause-screen");
 export const addScore = (points)=> {
     score += points;
+    scoreElement.innerText = score;
+}
+
+export const togglePause = ()=> {
+    isPaused = !isPaused;
+    if (isPaused) {
+        pauseScreen.classList.add("visible")
+    } else {
+        pauseScreen.classList.remove("visible")
+    }
+}
+
+export const getPauseState = ()=> {
+   return isPaused;
+}
+
+export const resetGameState = ()=> {
+    score = 0;
     scoreElement.innerText = score;
 }
