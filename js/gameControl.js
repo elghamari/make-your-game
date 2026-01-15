3e29b3921d953e1623aebc74171f14519945dc1c
elghamari <mohammed.elghamari@gmail.com>
2026-01-15 16:43:24 +0100

    create functions for cleaning main and keep code clear

---
+++ js/gameControl.js
@@ -0,0 +1,66 @@
+import { togglePause, getPauseState, resetGameState } from "./gameState.js";
+import { restAliens, createAliens } from "./createAliens.js";
+import { restLaser } from "./laser.js";
+import { initPlayerPosition, createPlayer } from "./movePlayer.js";
+import { createLives } from "./updateLives.js";
+import { createBunker } from "./createBunker.js";
+import { resetMoveAliens } from "./moveAliens.js"; 
+
+
+let totalGameTime = 0;  
+let startTime = null;   
+
+
+export const getGameTime = (currentTime) => {
+    if (startTime === null) {
+        startTime = currentTime;
+    }
+    const delta = currentTime - startTime;
+    startTime = currentTime;
+    if (!getPauseState()) {
+        totalGameTime += delta;
+    }
+
+    return totalGameTime;
+};
+
+export const handlePause = (e) => {
+    if (e && e.type === "keydown" && e.key.toLowerCase() !== "p") return; 
+
+    togglePause(); 
+    
+    if (!getPauseState()) {
+        startTime = null; 
+        document.getElementById("game-board").focus();
+    }
+};
+
+export const restartGame = () => {
+    if (getPauseState()) togglePause();
+    
+    totalGameTime = 0;
+    startTime = null; 
+    
+    restAliens();
+    restLaser();
+    resetGameState();
+    resetMoveAliens(); 
+    const bunkerContainer = document.getElementById("bunker-container");
+    if (bunkerContainer) bunkerContainer.innerHTML = "";
+    
+    initPlayerPosition(); 
+    createAliens();
+    createLives();
+    createBunker();
+    document.getElementById("game-board").focus();
+};
+
+export const initGame = () => {
+    totalGameTime = 0;
+    startTime = null;
+    createPlayer();
+    createAliens();
+    createLives();
+    createBunker();
+    initPlayerPosition();
+}
